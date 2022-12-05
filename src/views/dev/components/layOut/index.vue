0b28a053dc8733f1b6d32881967791a7e06ce886
zhoukaiyue <1301524439@qq.com>
2022-12-05 16:36:31 +0800

    docs: update layout组件使用说明文档

--- src/views/dev/components/layOut/index.vue
+++ src/views/dev/components/layOut/index.vue
@@ -4,14 +4,23 @@
  * @Author: zhoukai
  * @Date: 2022-09-29 16:37:24
  * @LastEditors: zhoukai
- * @LastEditTime: 2022-10-20 14:08:57
+ * @LastEditTime: 2022-12-05 16:33:19
 -->
 
 <template>
     <frame-view class="dev-layout">
-        布局组件:
+        基础布局组件是用来布局的，集成了tabbar（底部导航栏，用于在不同页面之间进行切换。）功能于一体。
         <br />
-        使用方法请参考 src/layOut 文件下面的README.md文件。
+        <br />
+        <span>什么时候使用？</span>
+        <br />
+        <span>如果你的src不是作为业务组件使用而是作为一个单独的页面，那么你应该使用它。</span>
+        <br />
+        <span>语法： 使用 "frame-view" 作为标签即可，就像当前页面这样。</span>
+        <br />
+        <br />
+        注：由于基础布局组件（'src/layout'）以及全局基础组件（'src/resources/components/*'）
+        默认全局自动导入了，所以不需要在sfc里面或者其他地方再次注册。
     </frame-view>
 </template>
 
@@ -42,6 +51,10 @@ export default {
         color: '#333';
         padding: 40px;
         font-size: 32px;
+        span {
+            font-size: 18px;
+            font-weight: bold;
+        }
     }
 }
 </style>
