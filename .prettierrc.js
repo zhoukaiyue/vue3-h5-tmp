3da74735762800fbe46a2ff1deb010b2f55af45c
zhoukaiyue <1301524439@qq.com>
2022-12-20 16:35:59 +0800

    build: 更新prettier配置
    解决prettier对超过print-width的HTML元素进行特殊的换行处理时引起的排版错乱bug。

--- .prettierrc.js
+++ .prettierrc.js
@@ -4,9 +4,8 @@
  * @Author: zhoukai
  * @Date: 2022-10-20 13:53:16
  * @LastEditors: zhoukai
- * @LastEditTime: 2022-10-20 14:03:37
+ * @LastEditTime: 2022-12-20 16:30:55
  */
-
 
 module.exports = {
     semi: true,
@@ -14,5 +13,6 @@ module.exports = {
     printWidth: 120,
     singleQuote: true,
     jsxSingleQuote: true,
-    trailingComma: 'none'
+    trailingComma: 'none',
+    htmlWhitespaceSensitivity: 'ignore' //所有空格被视为不敏感，皆采用「空格不敏感格式化」
 };
